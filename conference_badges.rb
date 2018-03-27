1ca38e39be45888fd70ab7c0ff3d98fd5262a5ee
Diego Yanez <diegosyanez92@gmail.com>
2018-03-27 20:44:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,7 @@ end
 
 def assign_rooms (attendees)
   new_array = []
+  counter = 1
     attendees.each do |name|
       new_rooms.push("Hello, #{name}! You'll be assigned to #{rooms -= 1}!")
     end
